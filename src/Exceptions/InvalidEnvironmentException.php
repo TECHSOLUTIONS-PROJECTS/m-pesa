d4454383cf716ebeed191c1eedf2913f8df3c1d2
Helio <heliomucumbi@gmail.com>
2024-09-26 08:58:46 +0200

    first commit

---
+++ src/Exceptions/InvalidEnvironmentException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Techsolutions\MPesa\Exceptions;
+
+use Exception;
+
+class InvalidEnvironmentException extends Exception
+{
+}
---
+++ src/Providers/MPesaServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Techsolutions\MPesa\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Techsolutions\MPesa\MPesa;
+
+class MPesaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publish the config file if running in console (for Laravel artisan)
+        // if ($this->app->runningInConsole()) {
+        //     $this->publishes([
+        //         __DIR__ . '/../../config/mpesa.php' => config_path('mpesa.php'),
+        //     ], 'config');
+        // }
+
+        $this->publishes([
+            __DIR__.'/../../config/mpesa.php' => config_path('mpesa.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Merge the package configuration with the app's configuration
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../config/mpesa.php', 'mpesa'
+        );
+
+        // Register the MPesa class with the application
+        $this->app->singleton('mpesa', function () {
+            return new MPesa();
+        });
+    }
+}
